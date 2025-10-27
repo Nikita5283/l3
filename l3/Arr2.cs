4da9a230bf8f219b24705e05d2fbb349cc0119f7
nikita <nikitaorlov59@gmail.com>
2025-10-27 19:18:16 +0500

    1.9.2

---
+++ l3/Arr2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l3
+{
+    internal class Arr2
+    {
+        private int[,] arr2;
+
+        public Arr2(int[,] arr2)
+        {
+            this.arr2 = arr2;
+        }
+
+        public void KeyboardArr()
+        {
+            int height = arr2.GetLength(0);
+            int width = arr2.GetLength(1);
+
+            Console.WriteLine("Массив введенный с клавиатуры(nxm): ");
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(arr2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void RandomArr()
+        {
+
+            int height = arr2.GetLength(0);
+            int width = arr2.GetLength(1);
+
+            Random random = new Random();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    int rnd_num = 0;
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            do
+                            {
+                                rnd_num = random.Next(1, 9);
+                            }
+                            while (rnd_num % 2 == 0);
+                        } 
+                        else
+                        {
+                            do
+                            {
+                                rnd_num = random.Next(1, 9);
+                            }
+                            while (rnd_num % 2 != 0);
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            do
+                            {
+                                rnd_num = random.Next(1, 9);
+                            }
+                            while (rnd_num % 2 != 0);
+                        }
+                        else
+                        {
+                            do
+                            {
+                                rnd_num = random.Next(1, 9);
+                            }
+                            while (rnd_num % 2 == 0);
+                        }
+                    }
+
+                    arr2[i, j] = rnd_num;
+                    
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Массив рандомных чисел(nxn): ");
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(arr2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+    }
+}
--- l3/Program.cs
+++ l3/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+namespace l3
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1.9
+            Console.WriteLine("1.9");
+            Console.Write("Введите высоту массива(n): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ширину массива(m): ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr2Dnm = new int[n, m];
+            int[,] arr2Dnn = new int[n, n];
+
+            Arr2 keyboard_arr = new Arr2(arr2Dnm);
+            keyboard_arr.KeyboardArr();
+
+            Arr2 random_arr = new Arr2(arr2Dnn);
+            random_arr.RandomArr();
+        }
+    }
+}
